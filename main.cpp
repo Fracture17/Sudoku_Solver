b44f4bc369d11893cd351a161ed8c852b9307df2
johno <johnotters@yahoo.com>
2020-05-10 21:00:41 -0700

    commit

--- main.cpp
+++ main.cpp
@@ -4,35 +4,40 @@
 #include "BruteForceBasicChecks.h"
 #include "Crooks.h"
 #include "Problems.h"
+#include <fstream>
 
 using namespace std;
 
 
-void timeSolver(Board (*solver)(Board& initialState)) {
+void timeSolver(Board (*solver)(Board& initialState), fstream& stream) {
     for (int i = 0; i < 50; ++i) {
         Board board(reinterpret_cast<short *>(hardBoards[i]));
-        board.display();
         auto start = std::chrono::high_resolution_clock::now();
 
         board = solver(board);
 
         auto end = std::chrono::high_resolution_clock::now();
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
-        cout << "Board " << i << " time: " << duration / 1000000.0 << " seconds" << endl;
-        board.display();
+        stream << "Board " << i << " time: " << duration / 1000000.0 << " seconds" << endl;
     }
 }
 
 int main() {
-    int threads = 6;
-    omp_set_num_threads(threads);
+    fstream crooksResults("crooksResults.txt", fstream::out);
+    fstream basicResults("basicResults.txt", fstream::out);
+    for(int threads = 1; threads <= 16; threads++) {
+        omp_set_num_threads(threads);
 
-    cout << "Crooks, threads=" << threads << endl;
-    timeSolver(solveCrooks);
-    cout << "Brute Force with basic checks, threads=" << threads << endl;
-    timeSolver(solveBruteForceWithBasicChecks);
-    cout << "Brute Force, threads=" << threads << endl;
-    timeSolver(solveBruteForce);
+        crooksResults << "Crooks, threads=" << threads << endl;
+        timeSolver(solveCrooks, crooksResults);
+
+        basicResults << "Brute Force with basic checks, threads=" << threads << endl;
+        timeSolver(solveBruteForceWithBasicChecks, basicResults);
+
+        //fstream bruteResults("bruteResults.txt", fstream::out);
+        //bruteResults << "Brute Force, threads=" << threads << endl;
+        //timeSolver(solveBruteForce, bruteResults);
+    }
 
     return 0;
 }
